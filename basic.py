5a0f7738e77a81d000281ea75ca44d0f3689480c
Ojo Happiness <olamidehappiness29@gmail.com>
2023-06-29 12:15:43 +0100

    Initial commit

---
+++ basic.py
@@ -0,0 +1,62 @@
+firstname = "Bola"
+lastname = " Winter"
+fullname = firstname + lastname
+print(fullname)
+
+name = "Bob"
+print("My name is " + name)
+
+#Type conversion....changing from one data type to another
+print(type(str(100)))
+print(type(int("20")))
+print(type(int(str(900))))
+
+girl_age = 10
+if girl_age < 15:
+    print(fullname + " is a teenager")
+else:
+    print("She is not a teenager")
+
+#Lists []
+list1 = ["one","three","four","five","six"]
+if "two" in list1:
+    print("Two is present in list1")
+else: 
+    print(list1[3] + " is present in list 1")
+list1.append("seven")
+list1.insert(1,"two")
+print(list1)
+# To append elements from another list to the current list, use the extend() method.
+# The extend() method does not have to append lists, you can add any iterable object (tuples, sets, dictionaries etc.).
+# The remove() method removes the specified item.
+# The pop() method removes the specified index.#
+# The clear() method empties the list.
+
+#looping through a list
+for lista in list1:
+    print(lista)
+    # the index is gotten
+for indexa in range(len(list1)):
+    print(indexa)
+    # while loop
+i = 0
+while(i<len(list1)):
+    print(list1[i])
+    i = i + 2
+
+# List objects have a sort() method that will sort the list alphanumerically, ascending, by default
+# To sort descending, use the keyword argument reverse = True:
+list1.sort(reverse=True)
+print(list1)
+
+#Tuples ()
+# You can convert the tuple into a list, change the list, and convert the list back into a tuple since a tuple is unchangeable.
+tuplea = ("one","two","three","four")
+listb = list(tuplea)
+listb[2] = "five"
+tuplea = tuple(listb)
+print(tuplea)
+
+#Adding a tuple to another tuple
+#SETS {}
+#A set is a collection which is unordered, unchangeable*, and unindexed.
---
+++ index.py
@@ -0,0 +1,86 @@
+# print("hello world");
+# if 2 > 5:
+#     print("5 is greater than 2");
+# else:
+#     print("2 is less than 5");
+#I USED MY KNOWLEDGE OF PHP ..... its a comment
+# x = 5
+# y = 6
+# print(x+y);
+# DATA TYPES
+# h = 8
+# print(type(h))
+#LET US LEARN ABOUT DATA TYPES
+J = "i AM A GIRL"  #string
+print(type(J))
+#multi line string
+u = ''' bob
+is
+a
+boy '''
+print(u)
+# k = 5 #integer
+# l = 70.5 #float
+# Python allows you to assign values to multiple variables in one line
+# b,v,c= "mimi","nini","lili"
+# print(v) 
+# And you can assign the same value to multiple variables in one line
+# s=d=f="one value to multiple variables"
+# print(d)
+
+#GLOBAL VARIABLES
+# Variables that are created outside of a function (as in all of the examples above) are known as global variables.
+# Global variables can be used by everyone, both inside of functions and outside.
+# s = "HELLO"
+
+# def myFunction():
+#     print(s + " TO YOU ALL")
+
+# myFunction()
+#Normally, when you create a variable inside a function, that variable is local, and can only be used inside that function.
+# To create a global variable inside a function, you can use the global keyword.
+
+#SRINGS ARE ARRAYS
+# y="Happy"
+# print(y[1])
+#looping through a string
+# for z in "apple":
+#     print(z)
+#To get the length of a string
+# q = "Olamide"
+# print(len(q))
+#To check if a certain phrase or character is present in a string, we can use the keyword in
+# text = "I am a Jesus kind of girl"
+# print("Jesus" in text)
+
+# python lists
+list=['one','two','three']
+print(list)
+
+#Python Boolean
+#Almost any value is true if it has some sort of content
+#Empty values are false
+print(bool("bannana"))
+print(bool())
+print(bool(None))
+print(bool(False))
+print(bool({}))
+
+#Python Lists
+#list is ordered and changeable. It allows duplicate members too.... negative index means from the end -1 refers to the last item -2
+#  refers to the second last item
+thislist = ["apple","banana","apple","guava","orange","cherry"]
+print(thislist)
+print(thislist[1])
+# len is used to determine how many items a list has
+# print(len(thislist))
+# print(thislist[2:5])
+
+#python Tuple is ordered and unchangeable and unindexed. Allows duplicate members....when creating a tuple with one item, 
+# add comma after the item
+thistuple = ("mango","pear","mango")
+print(thistuple)
+
+print(2+3)
+print(9-0)
+print(type(9/3))
